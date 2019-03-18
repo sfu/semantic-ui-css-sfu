064e9b3db4bcbe3212f099b2f987955155884af8
Graham Ballantyne <grahamb@sfu.ca>
2019-03-18 16:48:19 -0700

    Tooling to package to NPM

---
+++ src/semantic/publish.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const path = require("path");
+const pkg = require("../../package.json");
+
+const packageJson = JSON.stringify(
+  {
+    name: "@sfu/semantic-ui-css-sfu",
+    version: pkg.version,
+    description: "Semantic UI CSS-only theme for SFU CLF",
+    author: "Graham Ballantyne <grahamb@sfu.ca>",
+    main: "semantic.js",
+    license: "MIT",
+    repository: {
+      type: "git",
+      url: "git://github.com/sfu/semantic-ui-css-sfu"
+    },
+    dependencies: {},
+    devDependencies: {}
+  },
+  null,
+  2
+);
+
+console.log("Creating dist/package.json");
+fs.writeFileSync(path.resolve(__dirname, "dist/package.json"), packageJson);
